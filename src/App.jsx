7d64062694bbc22774fbbfe9c849497edd3ecb6d
huu-thien <86077619+huu-thien@users.noreply.github.com>
2023-06-22 23:18:10 +0700

    navbar w react-pro-sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -38,8 +38,8 @@ function App() {
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
               {/* <Route path="/faq" element={<FAQ />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
               {/* <Route path="/calendar" element={<Calendar />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
         </div>
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Typography, Box, useTheme } from '@mui/material';
+import { tokens } from '../theme';
+import PropTypes from 'prop-types';
+
+
+const Header = ({ title, subtitle }) => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <Box mb="30px">
+      <Typography
+        variant="h2"
+        color={colors.grey[100]}
+        fontWeight="bold"
+        sx={{ mb: '5px' }}
+      >
+        {title}
+      </Typography>
+      <Typography variant="h5" color={colors.greenAccent[400]}>
+        {subtitle}
+      </Typography>
+    </Box>
+  );
+};
+Header.propTypes =  {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired
+}
+export default Header;
