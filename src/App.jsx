48e8a1d097b82045f4589216312905bae8ae1693
huu-thien <86077619+huu-thien@users.noreply.github.com>
2023-06-22 17:45:20 +0700

    theme settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
-
-
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-     <h1>Hello</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content"></main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
